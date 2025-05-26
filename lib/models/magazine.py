f3bdf732dedba2b068226969a6fd3606be187828
Luke <luke.wangai@student.moringaschool.com>
2025-05-26 12:55:17 +0300

    stage 6 quiry selction data in lib/models

---
+++ lib/models/magazine.py
@@ -0,0 +1,55 @@
+from lib.db.connection import get_connection
+
+class Magazine:
+    def __init__(self, id, name, category):
+        self.id = id
+        self.name = name
+        self.category = category
+
+    @classmethod
+    def create(cls, name, category):
+        conn = get_connection()
+        cursor = conn.cursor()
+        cursor.execute(
+            "INSERT INTO magazines (name, category) VALUES (?, ?)",
+            (name, category)
+        )
+        conn.commit()
+        return cls(cursor.lastrowid, name, category)
+
+    def articles(self):
+        conn = get_connection()
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM articles WHERE magazine_id = ?", (self.id,))
+        return cursor.fetchall()
+
+    def contributors(self):
+        conn = get_connection()
+        cursor = conn.cursor()
+        cursor.execute("""
+            SELECT DISTINCT a.* FROM authors a
+            JOIN articles ar ON a.id = ar.author_id
+            WHERE ar.magazine_id = ?
+        """, (self.id,))
+        return cursor.fetchall()
+
+    def article_titles(self):
+        conn = get_connection()
+        cursor = conn.cursor()
+        cursor.execute("""
+            SELECT title FROM articles
+            WHERE magazine_id = ?
+        """, (self.id,))
+        return [row['title'] for row in cursor.fetchall()]
+
+    def contributing_authors(self):
+        conn = get_connection()
+        cursor = conn.cursor()
+        cursor.execute("""
+            SELECT a.*, COUNT(*) as article_count FROM authors a
+            JOIN articles ar ON a.id = ar.author_id
+            WHERE ar.magazine_id = ?
+            GROUP BY a.id
+            HAVING article_count > 2
+        """, (self.id,))
+        return cursor.fetchall()
